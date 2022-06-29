bd285aad5339883ccd9af9d210b6f909306f280a
Xila Luna <xilaluna2@gmail.com>
2022-06-28 19:35:22 -0700

    [add] createItem logic & add timestamps to model

--- server/controllers/itemController.js
+++ server/controllers/itemController.js
@@ -2,6 +2,7 @@ import Item from "../models/itemModel.js"
 
 export const getItems = async (req, res) => {
   try {
+    // use find method to get all data
     const items = await Item.find()
     res.status(200).json(items)
   } catch (error) {
@@ -11,7 +12,17 @@ export const getItems = async (req, res) => {
 
 export const createItem = async (req, res) => {
   try {
-    res.status(200).json("created item")
+    const item = req.body
+    const newItem = await Item.create({
+      name: item.name,
+      owner: item.owner,
+      price: item.price,
+      deposit: item.deposit,
+      description: item.description,
+      category: item.category,
+      condition: item.condition,
+    })
+    res.status(200).json(newItem)
   } catch (error) {
     res.status(400).json({ message: error.message })
   }
