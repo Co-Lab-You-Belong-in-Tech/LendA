867b656c8e21e6d65e8dfa88c9cc2e8dd81000ea
Xila Luna <xilaluna2@gmail.com>
2022-07-01 20:54:43 -0700

    [add] get user logic

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -60,7 +60,17 @@ export const login = async (req, res) => {
 
 export const getUser = async (req, res) => {
   try {
-    res.status(200).json(`got user ${req.params.id}`)
+    const user = await User.findById(req.params.id)
+
+    res.status(200).json({
+      success: true,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: user.createdAt,
+      },
+    })
   } catch (error) {
     res.status(400).json({ message: error.message })
   }
