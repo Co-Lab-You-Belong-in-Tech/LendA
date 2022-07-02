0b4a3e3e1ce3d666c0bc764bbad2e88eb7528779
Xila Luna <xilaluna2@gmail.com>
2022-07-01 21:06:00 -0700

    [update]user item models

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose"
 const userSchema = new mongoose.Schema(
   {
     name: { type: String },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
   },
   { timestamps: true }
