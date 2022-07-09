5fe8f1e6bdaef374378db8483ddc38d91b9c6718
Xila Luna <xilaluna2@gmail.com>
2022-07-09 00:04:57 -0700

    [update] redux structure

--- client/src/features/auth/authService.js
+++ client/src/features/auth/authService.js
@@ -1,38 +1,28 @@
-import axios from 'axios';
+import axios from "axios"
 
-const API_URL = 'http://localhost:8000/user'
+const API_URL = "http://localhost:8000/user"
 
 // Register user
 const register = async (userData) => {
-    const response = await axios.post(API_URL + '/register', userData)
-
-    if(response.data) {
-        localStorage.setItem('user', JSON.stringify(response.data))
-    }
-
-    return response.data
+  const response = await axios.post(API_URL + "/register", userData)
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data.data))
+  }
+  return response.data
 }
 
 // Login user
 const login = async (userData) => {
-    const response = await axios.post(API_URL + '/login', userData)
-
-    if(response.data) {
-        localStorage.setItem('user', JSON.stringify(response.data))
-    }
-
-    return response.data
-}
-
-// Logout user
-const logout = () => {
-    localStorage.removeItem('user')
+  const response = await axios.post(API_URL + "/login", userData)
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data.data))
+  }
+  return response.data
 }
 
 const authService = {
-    register,
-    logout,
-    login,
+  register,
+  login,
 }
 
 export default authService
