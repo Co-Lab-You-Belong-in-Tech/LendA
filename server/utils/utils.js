c57f9057fb8b0cb098d671466e0eb0e413762c4c
Xila Luna <xilaluna2@gmail.com>
2022-07-09 17:43:58 -0700

    [update] import utilsjs

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken"
-import { JWT_SECRET } from "./config/config.js"
+import { JWT_SECRET } from "../config/config.js"
 
 export const signJWT = async (id) => {
   const token = await jwt.sign({ sub: id }, JWT_SECRET, { expiresIn: "1d" })
