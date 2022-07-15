19791aa10d0715af7843e1dd8827e6e9a8f1f672
Mallory Daniel <mllr.dnl@gmail.com>
2022-07-14 20:18:44 -0700

    "removed social proof and ratings"

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -50,97 +50,6 @@ function Home() {
         </button>
       </div>
       {/* search bar end */}
-      {/* successful lend cards start */}
-      <div className="sLendsHeader">
-        <h2>Successful Lends</h2>
-      </div>
-
-      <div className="successfulLends">
-        <div className="sLendCard">
-          <div className="itemPic">
-            <img
-              src="https://media.istockphoto.com/vectors/thumbnail-image-vector-graphic-vector-id1147544807?k=20&m=1147544807&s=612x612&w=0&h=pBhz1dkwsCMq37Udtp9sfxbjaMl27JUapoyYpQm0anc="
-              alt="random placeholder"
-            ></img>
-          </div>
-          <div className="sLendDetails">
-            <p>
-              <strong>Nirmal super thanks Henry</strong>
-            </p>
-            <p>for lending a</p>
-            <p>
-              <strong>Laptop</strong>
-            </p>
-            <div className="timeLocation">
-              <p>10 minutes ago</p> <p>Seattle, WA</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="sLendCard">
-        <div className="itemPic">
-            <img
-              src="https://media.istockphoto.com/vectors/thumbnail-image-vector-graphic-vector-id1147544807?k=20&m=1147544807&s=612x612&w=0&h=pBhz1dkwsCMq37Udtp9sfxbjaMl27JUapoyYpQm0anc="
-              alt="random placeholder"
-            ></img>
-          </div>
-          <div className="sLendDetails">
-            <p>
-              <strong>Jenny super thanks Daisy</strong>
-            </p>
-            <p>for lending a</p>
-            <p>
-              <strong>Table</strong>
-            </p>
-            <div className="timeLocation">
-              <p>10 minutes ago</p> <p>San Diego, CA</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="sLendCard">
-          <div className="itemPic">
-              <img
-                src="https://media.istockphoto.com/vectors/thumbnail-image-vector-graphic-vector-id1147544807?k=20&m=1147544807&s=612x612&w=0&h=pBhz1dkwsCMq37Udtp9sfxbjaMl27JUapoyYpQm0anc="
-                alt="random placeholder"
-              ></img>
-            </div>
-            <div className="sLendDetails">
-              <p>
-                <strong>Xila super thanks Janey</strong>
-              </p>
-              <p>for lending a</p>
-              <p>
-                <strong>Wifi</strong>
-              </p>
-              <div className="timeLocation">
-                <p>10 minutes ago</p> <p>Oakland, CA</p>
-              </div>
-            </div>
-        </div>
-
-        <div className="sLendCard">
-        <div className="itemPic">
-            <img
-              src="https://media.istockphoto.com/vectors/thumbnail-image-vector-graphic-vector-id1147544807?k=20&m=1147544807&s=612x612&w=0&h=pBhz1dkwsCMq37Udtp9sfxbjaMl27JUapoyYpQm0anc="
-              alt="random placeholder"
-            ></img>
-          </div>
-          <div className="sLendDetails">
-            <p>
-              <strong>Mallory super thanks Sally</strong>
-            </p>
-            <p>for lending a</p>
-            <p>
-              <strong>Bamboo Steamer</strong>
-            </p>
-            <div className="timeLocation">
-              <p>10 minutes ago</p> <p>Portland, OR</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* successful lends cards end */}
 
       {/* items available start */}
       <div className="availItemsHeader">
@@ -161,16 +70,13 @@ function Home() {
               >
                 <div className="availItemPic">
                   <img
-                    src={item.image}
+                    src="https://via.placeholder.com/150"
                     alt="random placeholder"
                   ></img>
                 </div>
                 <div className="itemCardDetails">
                   <div className="itemCardRowOne">
                     <h3>{item.name}</h3>
-                    <p>
-                      4.96 <i className="fa-solid fa-star"></i>
-                    </p>
                   </div>
                   <div className="itemCardRowTwo">Location</div>
                   <div className="itemCardRowThree">
