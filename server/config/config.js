7c4f19c894a8e0279f38761f7da8a4a7e7e8565e
Mallory Daniel <mllr.dnl@gmail.com>
2022-07-25 12:08:27 -0700

    "images working"

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const PORT = process.env.PORT || 5000;
+export const PORT = process.env.PORT || 5001;
 export const NODE_ENV = process.env.NODE_ENV;
 
 export const CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING;
