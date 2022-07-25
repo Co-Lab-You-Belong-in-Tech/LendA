504e9b94626380fd067ec3ce8d8fdc44998f72eb
Xila Luna <xilaluna2@gmail.com>
2022-07-25 13:02:17 -0700

    [add] getCurrentUser to redux

--- client/src/features/auth/authSlice.js
+++ client/src/features/auth/authSlice.js
@@ -1,8 +1,8 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
-import authService from "./authService"
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import authService from './authService';
 
 // Get user from localStorage
-const currentUser = JSON.parse(localStorage.getItem("currentUser"))
+const currentUser = JSON.parse(localStorage.getItem('currentUser'));
 
 const initialState = {
   currentUser: currentUser ? currentUser : null,
@@ -10,113 +10,131 @@ const initialState = {
   isError: false,
   isSuccess: false,
   isLoading: false,
-  message: "",
-}
+  message: '',
+};
 
 // Register user
 export const register = createAsyncThunk(
-  "auth/register",
+  'auth/register',
   async (user, thunkAPI) => {
     try {
-      return await authService.register(user)
+      return await authService.register(user);
     } catch (error) {
       const message =
         (error.response &&
           error.response.data &&
           error.response.data.message) ||
         error.message ||
-        error.toString()
-      return thunkAPI.rejectWithValue(message)
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
     }
   }
-)
+);
 
 // Login user
 export const login = createAsyncThunk(
-  "auth/login",
+  'auth/login',
   async (currentUser, thunkAPI) => {
     try {
-      return await authService.login(currentUser)
+      return await authService.login(currentUser);
     } catch (error) {
       const message =
         (error.response &&
           error.response.data &&
           error.response.data.message) ||
         error.message ||
-        error.toString()
-      return thunkAPI.rejectWithValue(message)
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
     }
   }
-)
+);
+
+export const getCurrentUser = createAsyncThunk(
+  'auth/getCurrentUser',
+  async () => {
+    try {
+      const { token } = JSON.parse(localStorage.getItem('currentUser'));
+      return await authService.getCurrentUser(token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return message;
+    }
+  }
+);
 
 // Get User
 export const getUser = createAsyncThunk(
-  "auth/getUser",
+  'auth/getUser',
   async (id, thunkAPI) => {
     try {
-      return await authService.getUser(id)
+      return await authService.getUser(id);
     } catch (error) {
       const message =
         (error.response &&
           error.response.data &&
           error.response.data.message) ||
         error.message ||
-        error.toString()
-      return thunkAPI.rejectWithValue(message)
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
     }
   }
-)
+);
 
 // Update User
 export const updateUser = createAsyncThunk(
-  "auth/updateUser",
-  async (id, userData, thunkAPI) => {
+  'auth/updateUser',
+  async (input, thunkAPI) => {
     try {
-      const token = thunkAPI.getItem().auth.currentUser.token
-      return await authService.updateUser(id, userData, token)
+      const token = thunkAPI.getItem().auth.currentUser.token;
+      return await authService.updateUser(input.id, input.userData, token);
     } catch (error) {
       const message =
         (error.response &&
           error.response.data &&
           error.response.data.message) ||
         error.message ||
-        error.toString()
-      return thunkAPI.rejectWithValue(message)
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
     }
   }
-)
+);
 
 // delete user
 export const deleteUser = createAsyncThunk(
-  "auth/deleteUser",
+  'auth/deleteUser',
   async (id, thunkAPI) => {
     try {
-      const token = thunkAPI.getItem().auth.currentUser.token
-      return await authService.updateUser(id, token)
+      const token = thunkAPI.getItem().auth.currentUser.token;
+      return await authService.updateUser(id, token);
     } catch (error) {
       const message =
         (error.response &&
           error.response.data &&
           error.response.data.message) ||
         error.message ||
-        error.toString()
-      return thunkAPI.rejectWithValue(message)
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
     }
   }
-)
+);
 
 export const authSlice = createSlice({
-  name: "auth",
+  name: 'auth',
   initialState,
   reducers: {
     reset: (state) => {
-      state.isLoading = false
-      state.isError = false
-      state.message = ""
+      state.isLoading = false;
+      state.isError = false;
+      state.message = '';
     },
     logout: (state) => {
-      localStorage.removeItem("currentUser")
-      state.currentUser = null
+      localStorage.removeItem('currentUser');
+      state.currentUser = null;
     },
   },
   extraReducers: (builder) => {
@@ -124,84 +142,99 @@ export const authSlice = createSlice({
 
       // Register User
       .addCase(register.pending, (state) => {
-        state.isLoading = true
+        state.isLoading = true;
       })
       .addCase(register.fulfilled, (state, action) => {
-        state.isLoading = false
-        state.isSuccess = true
-        state.currentUser = action.payload.data
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.currentUser = action.payload.data;
       })
       .addCase(register.rejected, (state, action) => {
-        state.isLoading = false
-        state.isError = true
-        state.message = action.payload.message
-        state.currentUser = null
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload.message;
+        state.currentUser = null;
       })
 
       // Login User
       .addCase(login.pending, (state) => {
-        state.isLoading = true
+        state.isLoading = true;
       })
       .addCase(login.fulfilled, (state, action) => {
-        state.isLoading = false
-        state.isSuccess = true
-        state.currentUser = action.payload.data
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.currentUser = action.payload.data;
       })
       .addCase(login.rejected, (state, action) => {
-        state.isLoading = false
-        state.isError = true
-        state.message = action.payload.message
-        state.currentUser = null
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload.message;
+        state.currentUser = null;
+      })
+
+      // Get Current User
+      .addCase(getCurrentUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getCurrentUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.currentUser = action.payload.data;
+      })
+      .addCase(getCurrentUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload.message;
       })
 
       // Get User
       .addCase(getUser.pending, (state) => {
-        state.isLoading = true
+        state.isLoading = true;
       })
       .addCase(getUser.fulfilled, (state, action) => {
-        state.isLoading = false
-        state.isSuccess = true
-        state.user = action.payload.data
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload.data;
       })
       .addCase(getUser.rejected, (state, action) => {
-        state.isLoading = false
-        state.isError = true
-        state.message = action.payload.message
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload.message;
       })
 
       // Update User
       .addCase(updateUser.pending, (state) => {
-        state.isLoading = true
+        state.isLoading = true;
       })
       .addCase(updateUser.fulfilled, (state, action) => {
-        state.isLoading = false
-        state.isSuccess = true
-        state.currentUser = action.payload.data
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.currentUser = action.payload.data;
       })
       .addCase(updateUser.rejected, (state, action) => {
-        state.isLoading = false
-        state.isError = true
-        state.message = action.payload.message
-        state.currentUser = null
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload.message;
+        state.currentUser = null;
       })
 
       // delete User
       .addCase(deleteUser.pending, (state) => {
-        state.isLoading = true
+        state.isLoading = true;
       })
       .addCase(deleteUser.fulfilled, (state, action) => {
-        state.isLoading = false
-        state.isSuccess = true
-        state.currentUser = action.payload.data
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.currentUser = action.payload.data;
       })
       .addCase(deleteUser.rejected, (state, action) => {
-        state.isLoading = false
-        state.isError = true
-        state.message = action.payload.message
-        state.currentUser = null
-      })
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload.message;
+        state.currentUser = null;
+      });
   },
-})
+});
 
-export const { reset, logout } = authSlice.actions
-export default authSlice.reducer
+export const { reset, logout } = authSlice.actions;
+export default authSlice.reducer;
